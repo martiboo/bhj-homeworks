9e7a386e5292759a2a5b0c5e66eb0edca1b28bd6
martiboo <sanmarrino@gmail.com>
2019-07-07 14:08:32 +0300

    secon commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,19 +2,17 @@ let menuElements = document.getElementsByClassName("menu__link");
 
 let arr = Array.from(menuElements);
 for (i = 0; i < arr.length; i++) {
-    arr[i].onclick = showActive;
-    let argument = arr[i];
-    if (argument.onclick) {
-        showActive(argument);
-    }
+    let element = arr[i];
+    element.onclick = showActive;
  }
 
-let subMenuElements = menuElements.querySelectorAll("menu_sub");
-
-
-function showActive(argument) {
-    subMenuElements.classList.add("menu_active");
+function showActive(e) {
+    let clickedButton = e.target;
+    let subMenuElement = clickedButton.parentElement.querySelector(".menu_sub");
+   
+    if (subMenuElement !== null) {
+        subMenuElement.classList.toggle("menu_active");
+        return false;
+    } 
+    return true;
 }
-
-
-subMenuElements.onclick = showActive;
