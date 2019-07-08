6bb3c642cd92884e4688a99c3cc95c90adaf4d4b
martiboo <sanmarrino@gmail.com>
2019-07-08 20:15:05 +0300

    second commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,11 @@
 let seconds = document.getElementById('timer');
 
+let timerId = setInterval(countdown, 1000);
+
 function countdown(){
     seconds.textContent--;
     if (seconds.textContent <= 0) {
-        clearInterval(countdown);
+        clearInterval(timerId);
         alert(`Вы победили в конкурсе`);
     }
 }
-
-setInterval(countdown, 1000);
