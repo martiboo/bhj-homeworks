cf5f85a4335bf9d32c2a0489542827059c72687d
martiboo <sanmarrino@gmail.com>
2019-07-08 22:51:04 +0300

    updated slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,18 +1,30 @@
 let arrows = document.getElementsByClassName("slider__arrows");
+let sliderItems = document.getElementsByClassName("slider__item");
+let arrItems = Array.from(sliderItems);
+
+let currentIndex = 0;
+
+function clickArrow(e) {
+    let clickedArrow = e.target;
+    arrItems[currentIndex].classList.remove("slider__item_active");
+    
+    if (clickedArrow.classList.contains("slider__arrow_prev")) {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = (arrItems.length - 1);
+        }
+
+    } else {
+        currentIndex++;
+        if (currentIndex === arrItems.length) {
+            currentIndex = 0;
+        }
+    }
+    arrItems[currentIndex].classList.add("slider__item_active");
+}
 
 let arr = Array.from(arrows);
 for (i = 0; i < arr.length; i++) {
     let arrow = arr[i];
-    arrow.onclick = showActive;
- }
-
-function showActive(e) {
-    let clickedButton = e.target;
-    let subMenuElement = clickedButton.parentElement.querySelector(".slider__item");
-   
-    if (subMenuElement !== null) {
-        subMenuElement.classList.toggle("slider__item_active");
-        return false;
-    } 
-    return true;
+    arrow.onclick = clickArrow;
 }
