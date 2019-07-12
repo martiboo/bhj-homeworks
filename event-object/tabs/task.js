c6fb1821ef3d6451dd292a781511f18eff283143
martiboo <sanmarrino@gmail.com>
2019-07-12 20:22:56 +0300

    second tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,9 +1,29 @@
 let tabs = document.getElementsByClassName("tab");
 
-let arr = Array.from(tabs);
-for (i = 0; i < arr.length; i++) {
-    let element = arr[i];
-    element.addEventListener("click", function() {
-        element.classList.toggle("tab__content_active");
+let arrTabs = Array.from(tabs);
+
+for (i = 0; i < arrTabs.length; i++) {
+    let elementTab = arrTabs[i];
+    elementTab.addEventListener("click", function() {
+        let activeElementTab = elementTab.parentElement.querySelector(".tab_active");
+        
+        activeElementTab.classList.remove("tab_active");
+        elementTab.classList.add("tab_active");
+
+        let indexTab = arrTabs.indexOf(elementTab);
+
+
+        let tabNavigation = elementTab.parentElement;
+        let contents = tabNavigation.parentElement.querySelectorAll(".tab__content");
+
+        let activeElementContent = tabNavigation.parentElement.querySelector(".tab__content_active");
+        activeElementContent.classList.remove("tab__content_active");
+        
+        let arrContents = Array.from(contents);
+
+        let activeContent = arrContents[indexTab];
+     
+        activeContent.classList.add("tab__content_active");
+        
     })
 }
