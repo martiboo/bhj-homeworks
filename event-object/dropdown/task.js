2632e95570531e42dc6dc9072f3ee66e72396561
martiboo <sanmarrino@gmail.com>
2019-07-12 20:21:27 +0300

    second dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,20 +1,26 @@
- let menuElements = document.getElementsByClassName("dropdown__link");
+let menuElements = document.getElementsByClassName("dropdown__value");
 
- let arr = Array.from(menuElements);
- for (i = 0; i < arr.length; i++) {
-     let element = arr[i];
-     element.onclick = showActive;
-  }
- 
- function showActive(e) {
-     let clickedButton = e.target;
-     let subMenuElement = clickedButton.parentElement.querySelector(".dropdown__value");
-    
-     if (subMenuElement !== null) {
-         subMenuElement.classList.toggle("dropdown__list_active");
-         return false;
-     } 
-     return true;
- }
+let menuLinks = document.getElementsByClassName("dropdown__item");
 
- 
+let arrLinks = Array.from(menuLinks);
+for (i = 0; i < arrLinks.length; i++) {
+    let elementLink = arrLinks[i];
+    elementLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        let dropdownList = elementLink.parentElement;
+        let value = dropdownList.parentElement.querySelector(".dropdown__value");
+        let newValue = elementLink.querySelector(".dropdown__link");
+        value.textContent = newValue.textContent;
+        let dropdown = value.parentElement.querySelector(".dropdown__list");
+        dropdown.classList.remove("dropdown__list_active");
+    })
+}
+
+let arr = Array.from(menuElements);
+for (i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    element.addEventListener("click", function () {
+        let dropdown = element.parentElement.querySelector(".dropdown__list");
+        dropdown.classList.toggle("dropdown__list_active");
+    })
+}
