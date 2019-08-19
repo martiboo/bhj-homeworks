1e98b8fafdf8e5241a01d404f2179f7895174318
martiboo <sanmarrino@gmail.com>
2019-08-19 18:50:17 +0300

    third tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,30 +2,29 @@ let tip = document.querySelectorAll(".has-tooltip");
 
 let currentTooltip = null;
 
+let element = document.createElement('div');
+element.className = 'tooltip';
+document.body.appendChild(element);
+
 for (let i = 0; i < tip.length; i++) {
     let tooltipElement = tip[i];
 
     let tooltip = tooltipElement.getAttribute("title");
-    let rect = tooltipElement.getBoundingClientRect();
 
-    let element = document.createElement('div');
-    element.className = 'tooltip';
-    element.style.left = rect.left + "px";
-    element.style.top = rect.bottom + "px";
-    element.innerHTML = tooltip;
-    tooltipElement.appendChild(element);
-    
     tooltipElement.addEventListener("click", function (e) {
         e.preventDefault();
-        console.log(currentTooltip);
+        element.innerHTML = tooltip;
+        let rect = tooltipElement.getBoundingClientRect();
+        element.style.left = rect.left + "px";
+        element.style.top = rect.bottom + "px";
 
         if (currentTooltip != null) {
-            currentTooltip.style.display = "none";
+            element.style.display = "none";
         }
 
-        if (element != currentTooltip) {
+        if (tooltipElement != currentTooltip) {
             element.style.display = "block";
-            currentTooltip = element;
+            currentTooltip = tooltipElement;
         } else {
             currentTooltip = null;
         }
